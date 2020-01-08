b7440ea530c970f2d1db00a1c87970cfe0c8f443
martinodonnell <modo1997@hotmail.co.uk>
2020-01-08 18:19:57 +0000

    added skill question files for each skill

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
-import React,{Component,View} from 'react'
-import { TouchableOpacity, Text } from 'react-native';
+import React,{Component} from 'react'
+import { StyleSheet,TouchableOpacity, Text,View } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import * as Constants from '../component/Constants'
 
@@ -12,10 +12,42 @@ export default class extends Component {
 
   render() {
      return (
-         <TouchableOpacity style = {{ margin: 128 }} onPress = {()=> goToSkill(Constants.CAMPING)}>
-            <Text>Camping </Text>
-         </TouchableOpacity>
-            
+        <View style = {styles.container}>
+            <TouchableOpacity onPress = {()=> goToSkill(Constants.CAMPING)}>
+               <Text>Camping </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress = {()=> goToSkill(Constants.CAMPING)}>
+               <Text>Camping </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress = {()=> goToSkill(Constants.CAMPING)}>
+               <Text>Camping </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress = {()=> goToSkill(Constants.CAMPING)}>
+               <Text>Camping </Text>
+            </TouchableOpacity>
+         </View>
       )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 25,
+    padding: 10,
+    borderColor:'red',
+    borderRadius: 4,
+    borderWidth: 0.5,
+  },
+  nav: {
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  navItem: {
+    flex: 1,
+    alignItems: "center",
+    padding: 10
+  },
+  subNavItem: {
+    padding: 5
+  }
+});
