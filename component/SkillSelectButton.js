064ed27cf9cac4c8a5a6721a1c75b1996b64dda3
Martin <martin@Martins-MacBook-Pro.local>
2024-04-28 00:20:30 -0700

    center ish adventure skills buttons

--- component/SkillSelectButton.js
+++ component/SkillSelectButton.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
 import { Link } from "expo-router";
+import { RFPercentage } from "react-native-responsive-fontsize";
+
 export default class extends Component {
-
-
   render() {
-    const { skill, textColor } = this.props
+    const { skill, textColor } = this.props;
+
     return (
       <Link style={[styles.container, { backgroundColor: textColor }]} href={`/skills/${skill}`}>
         {this.props.skill}
@@ -18,14 +19,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     borderColor: 'black',
-    borderRadius: 10,
+    borderRadius: 15,
     borderWidth: 3,
-    justifyContent: 'center',
-    alignItems: 'center',
     marginTop: 5,
+    overflow: 'hidden',
     color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
+    paddingTop: RFPercentage(3.5),
     fontFamily: 'usuzi',
-    fontSize: 20
   },
 });
 
