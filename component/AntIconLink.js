d7f9f57c609bb106dde1a190996049a608113f46
Martin <modo1997@hotmail.co.uk>
2025-01-20 20:41:27 -0800

    delete file and move things

--- component/AntIconLink.js
+++ component/AntIconLink.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { TouchableOpacity, Linking } from "react-native";
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign } from "@expo/vector-icons";
 
 const openURL = (mobilieURL, websiteURL) => {
-    Linking.canOpenURL(mobilieURL).then(supported => {
-        if (supported) {
-            Linking.openURL(mobilieURL)
-        } else {
-            Linking.openURL(websiteURL)
-        }
-    })
-}
+  Lsdsdinking.canOpenURL(mobilieURL).then((supported) => {
+    if (supported) {
+      Linking.openURL(mobilieURL);
+    } else {
+      Linking.openURL(websiteURL);
+    }
+  });
+};
 
-const AntIconLink = ({name,iconSize,color, mobileURL, websiteURL}) => (
-    <TouchableOpacity onPress={() => openURL(mobileURL, websiteURL)}>
-        <AntDesign name={name} size={iconSize} color={color} />
-    </TouchableOpacity>
-)
+const AntIconLink = ({ name, iconSize, color, mobileURL, websiteURL }) => (
+  <TouchableOpacity onPress={() => openURL(mobileURL, websiteURL)}>
+    <AntDesign name={name} size={iconSize} color={color} />
+  </TouchableOpacity>
+);
 
-export {AntIconLink}
+export { AntIconLink };
